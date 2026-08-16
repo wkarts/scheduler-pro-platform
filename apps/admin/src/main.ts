38be4bc174d0141184dec3482ed1327050520404
wkarts <57051272+wkarts@users.noreply.github.com>
2026-08-16 19:01:01 -0300

    feat(admin): activate enterprise control plane with IAM and log console

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './AdminHub.vue'
+import App from './AdminControlPlane.vue'
 import './styles.css'
 import './operational.css'
 import './hubfiscal-admin.css'
+import './enterprise.css'
 import './pwa'
 
 createApp(App).use(createPinia()).mount('#app')
