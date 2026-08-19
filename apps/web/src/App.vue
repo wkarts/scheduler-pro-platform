c3bfb1285b6145acbc4170f30752eec600979cf0
wkarts <57051272+wkarts@users.noreply.github.com>
2026-08-19 16:56:14 -0300

    feat: mount tenant mail delivery mode selector

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -5,6 +5,7 @@ import TenantBrandAssetUploader from './TenantBrandAssetUploader.vue'
 import TenantBrandedLogin from './TenantBrandedLogin.vue'
 import TenantConsole from './TenantConsole.vue'
 import TenantExtensions from './TenantExtensions.vue'
+import TenantMailModeSelector from './TenantMailModeSelector.vue'
 
 const authenticated = ref(Boolean(localStorage.getItem('scheduler_pro_access_token')))
 let authPoll: number | undefined
@@ -29,6 +30,7 @@ onUnmounted(() => {
     <TenantConsole />
     <TenantExtensions />
     <TenantAgendaOperations />
+    <TenantMailModeSelector />
     <TenantBrandAssetUploader />
   </template>
 </template>
