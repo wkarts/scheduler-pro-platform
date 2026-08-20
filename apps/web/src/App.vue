ca47cf7871a152e3d5511acab7a17792cd830d32
wkarts <57051272+wkarts@users.noreply.github.com>
2026-08-20 09:43:08 -0300

    feat(pwa): manter instalador disponível antes e depois do login

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -7,6 +7,7 @@ import TenantBrandedLogin from './TenantBrandedLogin.vue'
 import TenantConsole from './TenantConsole.vue'
 import TenantExtensions from './TenantExtensions.vue'
 import TenantMailModeSelector from './TenantMailModeSelector.vue'
+import TenantPwaInstallSurface from './TenantPwaInstallSurface.vue'
 import TenantUniversalDownloads from './TenantUniversalDownloads.vue'
 
 const authenticated = ref(Boolean(localStorage.getItem('scheduler_pro_access_token')))
@@ -37,4 +38,5 @@ onUnmounted(() => {
     <TenantBrandAssetUploader />
     <TenantUniversalDownloads />
   </template>
+  <TenantPwaInstallSurface />
 </template>
