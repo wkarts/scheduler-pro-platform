948ebce23cc7b85e003a8e11d4cc43f5c620d97a
wkarts <57051272+wkarts@users.noreply.github.com>
2026-08-27 00:32:31 -0300

    feat(builder): ativar novo editor com fallback controlado

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -15,53 +15,41 @@ import TenantPublicPageEditorV2 from './TenantPublicPageEditorV2.vue'
 import TenantPwaInstallSurface from './TenantPwaInstallSurface.vue'
 import TenantSecondFactorGate from './TenantSecondFactorGate.vue'
 import TenantUniversalDownloads from './TenantUniversalDownloads.vue'
+import TenantVisualPageBuilder from './TenantVisualPageBuilder.vue'
 import TenantWorkspaceCoordinator from './TenantWorkspaceCoordinator.vue'
 import './tenantContrast.css'
 import './tenantEditorMobileHotfix.css'
 
-const authenticated = ref(Boolean(localStorage.getItem('scheduler_pro_access_token')))
-const normalizedPath = window.location.pathname.replace(/\/+$/, '') || '/'
-const publicSurface = ref(['/agendar', '/pagina'].includes(normalizedPath))
-
-function refreshAuthState(): void {
-  authenticated.value = Boolean(localStorage.getItem('scheduler_pro_access_token'))
-}
-
-function onStorage(event: StorageEvent): void {
-  if (event.key === 'scheduler_pro_access_token') refreshAuthState()
-}
-
-onMounted(() => {
-  refreshAuthState()
-  window.addEventListener('storage', onStorage)
-  window.addEventListener('scheduler-pro-auth-changed', refreshAuthState)
-})
-
-onUnmounted(() => {
-  window.removeEventListener('storage', onStorage)
-  window.removeEventListener('scheduler-pro-auth-changed', refreshAuthState)
-})
+const authenticated=ref(Boolean(localStorage.getItem('scheduler_pro_access_token')))
+const normalizedPath=window.location.pathname.replace(/\/+$/,'')||'/'
+const publicSurface=ref(['/agendar','/pagina'].includes(normalizedPath))
+const visualBuilderEnabled=import.meta.env.VITE_VISUAL_PAGE_BUILDER!=='false'
+function refreshAuthState():void{authenticated.value=Boolean(localStorage.getItem('scheduler_pro_access_token'))}
+function onStorage(event:StorageEvent):void{if(event.key==='scheduler_pro_access_token')refreshAuthState()}
+onMounted(()=>{refreshAuthState();window.addEventListener('storage',onStorage);window.addEventListener('scheduler-pro-auth-changed',refreshAuthState)})
+onUnmounted(()=>{window.removeEventListener('storage',onStorage);window.removeEventListener('scheduler-pro-auth-changed',refreshAuthState)})
 </script>
 
 <template>
-  <PublicSitePage v-if="publicSurface" />
+  <PublicSitePage v-if="publicSurface"/>
   <template v-else>
-    <TenantBrandedLogin v-if="!authenticated" @authenticated="refreshAuthState" />
+    <TenantBrandedLogin v-if="!authenticated" @authenticated="refreshAuthState"/>
     <template v-else>
-      <TenantConsole />
-      <TenantWorkspaceCoordinator />
-      <TenantDashboardInsights />
-      <TenantAgendaCenter />
-      <TenantAgendaOperator />
-      <TenantExtensions />
-      <TenantConfigurationCenter />
-      <TenantPublicPageEditorV2 />
-      <TenantBookingAndMessages />
-      <TenantMailModeSelector />
-      <TenantBrandAssetUploader />
-      <TenantUniversalDownloads />
-      <TenantSecondFactorGate />
+      <TenantConsole/>
+      <TenantWorkspaceCoordinator/>
+      <TenantDashboardInsights/>
+      <TenantAgendaCenter/>
+      <TenantAgendaOperator/>
+      <TenantExtensions/>
+      <TenantConfigurationCenter/>
+      <TenantVisualPageBuilder v-if="visualBuilderEnabled"/>
+      <TenantPublicPageEditorV2 v-else/>
+      <TenantBookingAndMessages/>
+      <TenantMailModeSelector/>
+      <TenantBrandAssetUploader/>
+      <TenantUniversalDownloads/>
+      <TenantSecondFactorGate/>
     </template>
-    <TenantPwaInstallSurface />
+    <TenantPwaInstallSurface/>
   </template>
 </template>
